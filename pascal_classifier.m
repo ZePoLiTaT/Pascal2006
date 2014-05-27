f381100b02b18d4f112fec6e4554c4bffe798450
Tatiana L <zepolitat@sirius.utp.edu.co>
2014-05-27 16:30:47 +0200

    Issue #8: Training using bounding box information Remove constrain of 50 sift features

--- pascal_classifier.m
+++ pascal_classifier.m
@@ -30,10 +30,10 @@ for i=1:VOCopts.nclasses
     
     
     % train classifier
-    classifier = train(VOCopts, cls, dictionary);                  
+    classifier = train_bounding_box(VOCopts, cls, dictionary);                  
     
     % test classifier
-    test(VOCopts,cls,classifier);                   
+    test(VOCopts,cls,dictionary,classifier);                   
     
     % compute and display ROC
     [fp,tp,auc]=VOCroc(VOCopts,'comp1',cls,true);   
@@ -61,6 +61,67 @@ function [ dictionary ] = load_dictionary( VOCopts, num_clusters )
     end
 
 
+% train classifier
+function classifier = train_bounding_box( VOCopts, cls, dictionary)
+
+    % load 'train' image set for class
+    [ids,classifier.gt] = textread(sprintf(VOCopts.clsimgsetpath,cls,'train'),'%s %d');
+
+    % Bounding box of the feature
+    bounding_box = {};
+    
+    % extract features for each image
+    classifier.FD=zeros(0,length(ids));
+    tic;
+    for i=1:length(ids)
+
+        % display progress
+        if toc > 1
+            fprintf('%s: train: %d/%d\n',cls,i,length(ids));
+            drawnow;
+            tic;
+        end
+        
+        % read annotation for the given train image
+        rec = PASreadrecord(sprintf(VOCopts.annopath,ids{i}));
+        
+        % find objects of class and extract difficult flags for these objects
+        
+        diff = [rec.objects.difficult];
+
+        % extract bounding boxes for non-difficult objects    
+        bounding_box{end+1} = cat( 1 , rec.objects(~diff).bbox)';
+        
+        try
+            % try to load features
+            load(sprintf(VOCopts.exfdpath,ids{i}),'fd');
+        catch
+            
+            % compute and save features
+            I  = imread(sprintf(VOCopts.imgpath,ids{i}));
+            fd = [];
+            
+            %Extract features on the patches of the bounding boxes
+            for j=1: size( bounding_box{end}, 2)
+                cur_box = bounding_box{end}(:,j);
+                img_box = I( cur_box(2):cur_box(4), cur_box(1):cur_box(3), : );
+
+                fd_box = sift_descriptor( img_box );
+                
+                fd = [ fd,  fd_box ];
+            end
+            
+            fd = make_histogram_3( fd, dictionary );
+            save(sprintf(VOCopts.exfdpath,ids{i}),'fd');
+        end
+        
+        classifier.FD( 1:length(fd) , i ) = fd';
+        
+    end
+classifier = weka_classifier( classifier.FD', classifier.gt );
+    
+    
+    
 % train classifier
 function classifier = train( VOCopts, cls, dictionary)
 
@@ -98,6 +159,7 @@ function classifier = train( VOCopts, cls, dictionary)
     end
 classifier = weka_classifier( classifier.FD', classifier.gt );
     
+
 function histogram = make_histogram( image_feat, dict_feat )
    
     histogram = zeros( 1, size( dict_feat, 2 ) );
@@ -134,13 +196,24 @@ function histogram = make_histogram_2( image_feat, dict_feat )
         end
     end
 
+function histogram = make_histogram_3( features, dictionary )
+   
+    histogram = zeros( 1, size( dictionary, 2 ) );
+    
+    for i = 1:size(features,2)
+        fd = features(:,i);
+        d = sum(fd.*fd)+sum(dictionary.*dictionary)-2*fd'*dictionary;
+        [d_min, ix_min] = min(d);
+        histogram(ix_min) = histogram(ix_min) + 1;
+    end
+        
     
 % return L2 norm as the similarity measurement
 function d = similarity_measurement(f1, f2)
     d = norm( f1 - f2 );
 
 % run classifier on test images
-function test(VOCopts,cls,classifier)
+function test(VOCopts,cls,dictionary,classifier)
 
 % load test set ('val' for development kit)
 [ids,gt]=textread(sprintf(VOCopts.clsimgsetpath,cls,VOCopts.testset),'%s %d');
@@ -164,7 +237,7 @@ for i=1:length(ids)
     catch
         % compute and save features
         I=imread(sprintf(VOCopts.imgpath,ids{i}));
-        fd=extractfd(VOCopts,I);
+        fd = extractfd( VOCopts, I, dictionary);
         save(sprintf(VOCopts.exfdpath,ids{i}),'fd');
         
     end
@@ -211,21 +284,18 @@ function fd = extractfd(VOCopts, I, dictionary)
     % fd = [fd; sift_descriptor(I)];
     
     fd = sift_descriptor(I);
-    fd = make_histogram( fd, dictionary );
+    fd = make_histogram_3( fd, dictionary );
     %[xx,yy] = stairs(histogram);
     %area(xx,yy);
 
 
 %sift feature extractor
-function fd = sift_descriptor(I)
+function descriptors = sift_descriptor(I)
 
     I = double(rgb2gray(I)/256) ;
-    [frames,descriptors] = sift(I, 'Verbosity', 1) ;
+    [~,descriptors] = sift(I, 'Verbosity', 1) ;
 
-    perm = randperm( size(frames,2) ) ;
-    sel  = perm(1:50);
-
-    fd = descriptors(:,sel);
+    %fd = descriptors;
     %fd = fd(:);
 
     % clf; imagesc(I) ; colormap gray ; axis image ; hold on ;
