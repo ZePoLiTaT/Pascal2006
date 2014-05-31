b76e2eb10a772bec2d31f1628bed931f92193bb1
Tatiana L <zepolitat@sirius.utp.edu.co>
2014-05-31 22:00:52 +0200

    Issue #9: Test with spare sift again Added sift plot

--- feature_analysis.m
+++ feature_analysis.m
@@ -8,17 +8,17 @@ addpath([cd '/VOCcode']);
 VOCinit;
 
 % Sizes for SPARSE SIFT
-%  dict_sizes = [100:100:1000];
+ dict_sizes = [100:100:1000];
 %  dict_sizes = 100;
 
 % Sizes for DENSE SIFT
- dict_sizes = [100,300,600,900];
+%  dict_sizes = [100,300,600,900];
 
 % create an AUC results table 
 results_auc = zeros( VOCopts.nclasses, length(dict_sizes) );
 
 for i = 1: length(dict_sizes)
-    results_auc(:, i) = pascal_classifier( VOCopts, dict_sizes(i), false );
+    results_auc(:, i) = pascal_classifier( VOCopts, dict_sizes(i), true );
 end
 
 latex_table( VOCopts.classes, results_auc, dict_sizes );
--- functions/test_sift.m
+++ functions/test_sift.m
@@ -5,10 +5,47 @@ bycicle = '000013';
 cow = '000019';
 
 img_src = imread(sprintf('VOC2006/PNGImages/%s.png',cow) );
+
+
+% img = img_box;
+figure(1); clf; imagesc(img); colormap gray ; axis image ; hold on ;
+
+% ----------------- SPARSE SIFT - ANDREA
+img = double( rgb2gray(img) )/256 ;
+[f, fd_sift] = sift(img, 'Verbosity', 0); 
+
+perm = randperm(size(f,2)) ;
+sel = 1:size(f,2); %perm(1:400);
+h1 = vl_plotframe(f(:,sel)) ;
+h2 = vl_plotframe(f(:,sel)) ;
+set(h1,'color','k','linewidth',3) ;
+set(h2,'color','y','linewidth',2) ;
+
+h=plotsiftframe( f(:,sel),'style','arrow' ) ; set(h,'LineWidth',1,'Color','g') ;
+h=plot(f(1,sel),f(2,sel),'r.');
+
+
+% ----------------- SPARSE SIFT - VLFEAT
+img = img_src;
+%test....
+% img = img_box;
+
+figure(1); clf; imagesc(img); colormap gray ; axis image ; hold on ;
+img = single(rgb2gray(img)) ;
+[f, d] = vl_sift(img) ;
+perm = randperm(size(f,2)) ;
+sel = 1:size(f,2); %perm(1:100) ; %
+h1 = vl_plotframe(f(:,sel)) ;
+h2 = vl_plotframe(f(:,sel)) ;
+set(h1,'color','k','linewidth',3) ;
+set(h2,'color','y','linewidth',2) ;
+%h3 = vl_plotsiftdescriptor(d(:,sel),f(:,sel)) ;
+%set(h3,'color','g') ;
+
+
+% ----------------- DENSE SIFT - VLFEAT
+img = img_src;
 img = single(vl_imdown(rgb2gray(img_src))) ;
-
-figure(1); imagesc(img);
-
 binSize = 8 ;
 magnif = 3 ;
 Is = vl_imsmooth(img, sqrt((binSize/magnif)^2 - .25)) ;
@@ -18,10 +55,8 @@ Is = vl_imsmooth(img, sqrt((binSize/magnif)^2 - .25)) ;
 
 f(3,:) = binSize/magnif ;
 f(4,:) = 0 ;
- %[f_, d_] = vl_sift(img, 'frames', f) ;
+ 
 
-figure(1); imagesc(Is);
-hold on;
 perm = randperm(size(f,2)) ;
 sel = 1:size(f,2); %perm(1:50) ;
 h1 = vl_plotframe(f(:,sel)) ;
